03a9657c16821ecf71d12e45fceda6d827a4c31b
Hrishabh963 <hrishabhkumar634@gmail.com>
2021-04-30 18:46:16 +0530

    made changes

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
-const config = require('./config.json')
+//const config = require('./config.json')
 
 client.on('ready', ()  => {
     console.log("Bot is ready")
 })
-client.login(config.token)
+client.login(process.env.TOKEN)
